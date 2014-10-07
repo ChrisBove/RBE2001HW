0bdf241bbeddff26a1a2ad9b1a95cfda068470f3
ChrisBove <bove.3123@gmail.com>
2014-10-07 15:22:55 -0400

    comments and optimizations

--- HW6/HW6.ino
+++ HW6/HW6.ino
@@ -3,13 +3,13 @@
         HW6 Solution
         
     Author: Christopher Bove
-    Date Finished: 10/6/2014
+    Date Finished: 10/7/2014
     
     Description:
-      This program displays a clock using the Liquid Crystal Display library based on the
-      internal Arduino timer. The format is hh:mm:ss
+      This program receives a TTL (0-5V square) waveform and calculates the average frequency of the waveform
+      over that period of time. The frequency is then printed on the first line of an LCD display.
+      The period is selectable through a pull-down pin at either 500ms or 100ms.
       
-      * A NO pushbutton is used on pin 2 to ground to reset the system.
       
       * Pins for the LCD are configured and defined as seen below the imported libraries.
       
@@ -18,6 +18,16 @@
       * TimerOne
       * stdio
     
+    
+    ANSWER TO EXTRA CREDIT QUESTION: 
+      The lowest frequency that can be reasonably detected in 500ms is roughly 2Hz. At that frequency,
+    the chip recieves approximately 1 interrupt per period. This allows a rough approximation to be made,
+    of 2Hz, for the frequency of that signal.
+      The lowest frequency for the 100ms period is 10Hz. Again, this would be 1 click per time period. 
+      
+    For both of these tests, it was experimentally determined, and the results make sense due to the
+    averaging nature of the calculation. The board received too many interrupts for frequencies around
+    1MHz to show the calculated frequency, so the range is approximately 2Hz to 870kHz.
     
 */
 
@@ -36,6 +46,7 @@
 #define LCD_DATA5    9
 #define LCD_DATA6    8
 #define LCD_DATA7    7
+// interrupt pins
 #define freqInt      0
 #define selectInt    1
 
@@ -49,26 +60,30 @@
 LiquidCrystal lcd(LCD_REGSEL, LCD_EN, LCD_DATA4, LCD_DATA5, LCD_DATA6, LCD_DATA7);
 
 // create variables used by the interrupts
-volatile int periodState = SHORT_PERIOD;
-volatile bool isPeriodChanged = true;
-volatile long unsigned int queuedFreqCount = 0;
-volatile unsigned int timeCount = 0;
-volatile int subCount = 0;
+volatile int periodState = SHORT_PERIOD; // to store period
+volatile bool isPeriodChanged = true;    // flag for period changes
+volatile long unsigned int queuedFreqCount = 0; // count of frequency rises
+volatile unsigned int timeCount = 0;  // flag to process frequency
+volatile int subCount = 0;   // count for 500ms period option
 
 // --------------------- SETUP ----------------------------
 void setup() {
   // initialize lcd with 16 columns, 2 rows
   lcd.begin(16, 2);
+  // set static label "Hz" to display
+  lcd.setCursor(14,0);
+  lcd.print("Hz");
+  lcd.setCursor(0,0);
   
-  // configure switch
+  // configure switch for period
   pinMode(periodSelectPin, INPUT_PULLUP);
   attachInterrupt(selectInt, selectPeriodISR, CHANGE);
   
-  // configure input
+  // configure input for frequency
   pinMode(inputPin, INPUT);
-  attachInterrupt(freqInt, freqISR, RISING);
+  attachInterrupt(freqInt, freqISR, RISING); // trigger on rising edge
   
-  // initialize period to start
+  // initialize the period to start - otherwise starting it low won't do anything
   selectPeriodISR();
   
   // configure timer
@@ -85,11 +100,11 @@ void freqISR() {
 
 // ISR for switching the period
 void selectPeriodISR() {
-  isPeriodChanged = true;
+  isPeriodChanged = true; // flag change
   if (digitalRead(periodSelectPin) == HIGH)
-    periodState = SHORT_PERIOD;
+    periodState = SHORT_PERIOD; // set to short period if pin is high
   else
-    periodState = LONG_PERIOD;
+    periodState = LONG_PERIOD; // set to long period if pin is low
 }
 
 // service routine for the 100msecond timer
@@ -101,7 +116,7 @@ void timerISR() {
   else { // 100, 200, 300, 400, 500
     // otherwise, just count every five to get 500ms counts
     if(subCount >= 4) {
-      timeCount ++;
+      timeCount ++; // its been 500ms, update count and reset subcount
       subCount = 0;
     }
     else
@@ -127,27 +142,21 @@ void drawPeriodStats() {
 
 // returns float of the calculated frequency of the signal
 float calcFreq() {
-  // grab the newest time count, freq count, and period
+  // grab the newest time count, freq count, and period w/o interrupts
   noInterrupts();
-  float tempTimeCount = timeCount;
+  // use doubles and floats to get unfloored math
   double tempFreqCount = queuedFreqCount;
   float tempPeriod = periodState;
   timeCount = 0; // reset time count - it has been serviced
   queuedFreqCount = 0; // reset frequency count
   interrupts();
-  // period = counts/1000, avgT = counts/period, f = 1/T
-  return (1000.0*tempFreqCount)/tempPeriod; //1.0/(tempTimeCount/(1000*tempPeriod)); 
+  // calculate the frequency
+  return (1000.0*tempFreqCount)/tempPeriod; 
 }
 
 // draws the passed frequency to line 1
 void drawFrequency(float toWrite) {
-  char tempString[17];
-  // convert to a nice string
-  sprintf(tempString, "Hz: %1.2f", toWrite);//toWrite);
-  lcd.print(toWrite); // print string
-  lcd.setCursor(14,0);
-  lcd.print("Hz");
-  lcd.setCursor(0,0);
+  lcd.print(toWrite); // print float
 }
 
 // checks if the period stats need to be refreshed
@@ -170,5 +179,4 @@ void loop() {
   checkPeriodSelect();
   // check if we have new ms, and if so calc freq. and update display
   checkTimerCount();
-
 }
