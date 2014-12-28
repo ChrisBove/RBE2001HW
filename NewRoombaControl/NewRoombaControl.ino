b149044508f872b904e669cc4240fee88d0b0a20
ChrisBove <bove.3123@gmail.com>
2014-12-28 13:44:39 -0500

    instantiate roomba class, add checks

--- NewRoombaControl/NewRoombaControl.ino
+++ NewRoombaControl/NewRoombaControl.ino
@@ -1,8 +1,31 @@
 #include "new_roomba.h"
 
+NewRoomba roomba(&Serial1, NewRoomba::Baud115200);
+
+unsigned int errors = 0;
+
+void error(const char *m)
+{
+  Serial.print("Error: ");
+  Serial.println(m);
+  errors++;
+}
+
+void check(boolean t, const char *m)
+{
+  if (!t)
+    error(m);
+}
+
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  
+  roomba.start();
+  roomba.fullMode();
+  
 }
 
 void loop() {
